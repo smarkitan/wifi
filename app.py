4610824a287e0b8146b0baca92630793320c7de4
smarkitan <97811066+smarkitan@users.noreply.github.com>
2024-12-09 17:04:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -115,4 +115,5 @@ def index():
     return render_template('index.html', wifi_details=wifi_details, devices=devices)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
