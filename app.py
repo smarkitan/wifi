b9b2d03620f70c54b36e0fe852d3604b365d2710
smarkitan <97811066+smarkitan@users.noreply.github.com>
2024-12-13 17:47:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -188,5 +188,4 @@ def ping_endpoint():
         })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
